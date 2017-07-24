af980527331bd11fa956385cc266400d06623a37
Gaeddar <ingenbryrsigomdenjavlaemailen@skitpadig.nu>
2017-07-24 22:25:04 +0200

    Addon
    more mascots

--- themes/ruri-dark/mascots.js
+++ themes/ruri-dark/mascots.js
@@ -1,1 +1,1 @@
-var mascotList = [ "serg.mad.png", "serg.GetIn.png", "serg.No.png", "serg.Lick.png", "serg.ah.png", "serg.Ye.png", "serg.bath.png", "serg.Dom.png", "serg.Sub.png", "serg.joinme.png", "serg.Hoi.png", "serg.OhHi.png", "serg.plz.png", "serg.serg.png", ];
+var mascotList = [ "serg.mad.png", "serg.GetIn.png", "serg.No.png", "serg.Lick.png", "serg.ah.png", "serg.Ye.png", "serg.bath.png", "serg.Dom.png", "serg.Sub.png", "serg.joinme.png", "serg.Hoi.png", "serg.OhHi.png", "serg.plz.png", "serg.serg.png", "serg.Serius.png", "serg.Purpelneck.png", "serg.Turqoise.png",  ];
